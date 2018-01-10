20a03ca0d490a3516d2d8ffaa98d92421618d182
Mateo Balcorta <misterrattail@gmail.com>
2018-01-09 16:49:08 -0800

    Remove promises and catch clauses.

--- Models/db/queries.js
+++ Models/db/queries.js
@@ -1,27 +1,19 @@
-const db = require('./db')
+const { db } = require('./db')
 
 const createTask = (task) => {
-  return db.query('INSERT INTO tasks(task) VALUES($1) RETURNING *', [task])
-    .then(console.log)
-    .catch(console.error)
+  return db.one('INSERT INTO tasks(task) VALUES($1) RETURNING *', [task])
 }
 
 const updateTask = (id, complete) => {
   return db.one('UPDATE tasks SET complete=$2 WHERE id=$1 RETURNING *', [id, complete])
-    .then(console.log)
-    .catch(console.error)
 }
 
 const editTask = (id, task) => {
   return db.one('UPDATE tasks SET task=$2 WHERE id=$1 RETURNING *', [id, task])
-    .then(console.log)
-    .catch(console.error)
 }
 
 const deleteTask = (id) => {
   return db.oneOrNone('DELETE FROM tasks WHERE id=$1 RETURNING *', id)
-    .then(console.log)
-    .catch(console.error)
 }
 
 module.exports = { createTask, updateTask, editTask, deleteTask }
