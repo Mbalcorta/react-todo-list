11ede5c461078e2741490c978f8506ff9ce4b0f8
ontoneio <ontoneio.mm@gmail.com>
2018-01-11 07:59:25 -0800

    Remove # from singup / login links

--- src/client/components/User-onboarding/index.js
+++ src/client/components/User-onboarding/index.js
@@ -4,8 +4,8 @@ class UserOnboarding extends Component {
   render() {
     return (
       <div className="user-onboarding-box">
-          <a href="#"><h4>Sign-up</h4></a>
-          <a href="#"><h4>Login</h4></a>
+          <a href=""><h4>Sign-up</h4></a>
+          <a href=""><h4>Login</h4></a>
       </div>     
     )
   }
