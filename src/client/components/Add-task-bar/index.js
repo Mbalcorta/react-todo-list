a2eb6cb923ebd9cba9ff56cf3e32b9cfa6f79175
ontoneio <ontoneio.mm@gmail.com>
2018-01-12 15:01:00 -0800

    adds styling to AddTaskBar

--- src/client/components/Add-task-bar/index.js
+++ src/client/components/Add-task-bar/index.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 
+const styles = {
+  bigBar:{
+    height: '85px',
+    width: '600px'
+  }}
+
 class AddTaskBar extends Component {
+  
+
+
+
   render() {
+    
+
     return (
-      <div className="add-task-bar">
+      <div className="add-task-bar" style={styles.bigBar}>
           <input type="text" value="" placeholder="Add task description here"/>
           <button name="add-task">+</button>
       </div>  
