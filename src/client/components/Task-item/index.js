dc6d5a12b3084756ed67dc627dee59de06e5642e
ontoneio <ontoneio.mm@gmail.com>
2018-01-19 14:41:25 -0800

    Convert TaskItem component to functional component and add taskDescription prop type

--- src/client/components/Task-item/index.js
+++ src/client/components/Task-item/index.js
@@ -1,18 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class TaskItem extends Component {
-  render() {
-    return (
-      <li className="">           
-        <div className="task-item-wrapper">
-          <button name="complete-task-btn">Done</button>
-          <input type="text" placeholder="Task that's supposed to happen"/>
-          <button name="remove-task-btn">X</button>
-          <button name="save-edit-btn">Save</button>
-        </div>
-      </li> 
-    )
-  }
+function TaskItem(props) {  
+  return (
+    <li className="">           
+      <div className="task-item-wrapper">
+        <button name="complete-task-btn">Done</button>
+        <input type="text" placeholder="Task that's supposed to happen"/>
+        <button name="remove-task-btn">X</button>
+        <button name="save-edit-btn">Save</button>
+      </div>
+    </li> 
+  )  
+}
+
+TaskItem.propTypes = {
+  taskDescription: React.PropTypes.string,
 }
 
 export default TaskItem;
